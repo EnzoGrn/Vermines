3e4c2ec34135fd3ae345103cdd854098d19e9f1f
jovanHillion <jovan.hillion@epitech.eu>
2024-10-10 14:50:19 +0200

    feat(lobby): add room creation logic
    Add room creation logic, containing lobbyManager to connect to master server, room manager to create and join room and waiting room manager to wait for the host to start the game.
    
    Feat #7,#9

--- Vermines/Assets/Scripts/Lobby/LobbyManager.cs
+++ Vermines/Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,26 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : ALobbyManager
 {
-    // Start is called before the first frame update
+    #region [SerializeField] Private Attributes
+    /*
+     * @brief This attribute is used to display a message to the client.
+     */
+    [SerializeField] private TMP_Text _messageText;
+
+    /*
+     * @brief This attribute is used to store the name of the menu scene, to potentially return to the main menu of the game.
+     */
+    [SerializeField] private string _menuSceneName;
+    #endregion
+
+    /*
+     * @brief This method is used to start the client connection to the server before Update method is called the first time..
+     * 
+     * @param void
+     * 
+     * @return void
+     */
     void Start()
     {
-        
+        _networkLobbyManagerPrefab.ConnectClientToServer();
     }
 
-    // Update is called once per frame
-    void Update()
+    #region NetworkManagerCallbacks
+    public override void OnClientConnectedToServer()
     {
-        
+        _messageText.text = "Client connected to server";
+        _messageText.color = Color.green;
     }
+    public override void OnClientDisconnectedFromServer()
+    {
+        _messageText.text = "Client disconnected to server";
+        _messageText.color = Color.red;
+        SceneManager.LoadScene(_menuSceneName, LoadSceneMode.Single);
+    }
+    #endregion
 
     #region Events
-    public void OnMenuButtonPressed()
+    /*
+     * @brief This method is used to handle the event when the client clicks on the menu button, it disconnect the player from the server.
+     * 
+     * @param void
+     * 
+     * @return void
+     */
+    public void OnClickMenuButton()
     {
-        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        _networkLobbyManagerPrefab.DisconnectClientFromServer();
     }
     #endregion
 }
---
+++ Vermines/Assets/Scripts/WaitingRoom/WaitingRoomManager.cs
@@ -0,0 +1,135 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WaitingRoomManager : AWaitingRoomManager
+{
+    #region [SerializeField] Private Attributes
+    /*
+     * @brief This attribute is used to store the game scene to load.
+     */
+    [SerializeField] private string _gameSceneToLoad; // TODO
+
+    /*
+     * @brief This attribute is used to store the lobby scene to load.
+     */
+    [SerializeField] private string _lobbyScene;
+
+    /*
+     * @brief This attribute is used to store and display the code of the actual room.
+     */
+    [SerializeField] private TMP_Text _roomCodeText;
+
+    /*
+     * @brief This attribute is used to store and display the total player in the room.
+     */
+    [SerializeField] private TMP_Text _totalPlayerText;
+
+    /*
+     * @brief This attribute is used to store and display the players name in the room.
+     */
+    [SerializeField] private TMP_Text _playersNameText;
+
+    /*
+     * @brief This attribute is used to store the start match button.
+     */
+    [SerializeField] private Button _startMatchButton;
+
+    #endregion
+
+    void Start()
+    {
+        SetUpStartMatchButton();
+        DisplayRoomCode();
+        DisplayTotalPlayer();
+        DisplayPlayersName();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /*
+     * @brief This method is used to setUp the state of the start match button, if the client isn't the master one he won't be able to interact with it.
+     * 
+     * @param void
+     * 
+     * @return void
+     */
+    private void SetUpStartMatchButton()
+    {
+        if (_networkWaitingRoomManager.IsMasterClient())
+        {
+            _startMatchButton.interactable = true;
+            _startMatchButton.enabled = true;
+        }
+        else
+        {
+            _startMatchButton.interactable = false;
+            _startMatchButton.enabled = false;
+            _startMatchButton.GetComponentInChildren<TMP_Text>().text = "Waiting for host...";
+        }
+    }
+
+    /*
+     * @brief This method is used to set the room code to the room code text.
+     * 
+     * @param void
+     * 
+     * @return void
+     */
+    private void DisplayRoomCode()
+    {
+        _roomCodeText.text = "Room Code : " + _networkWaitingRoomManager.GetRoomCode();
+    }
+
+
+    /*
+     * @brief This method is used to set the total player to the total player text.
+     * 
+     * @param void
+     * 
+     * @return void
+     */
+    private void DisplayTotalPlayer()
+    {
+        _totalPlayerText.text = "Total Players : " + _networkWaitingRoomManager.GetPlayerCount() + "/" + _networkWaitingRoomManager.GetMaxPlayersRoom();
+    }
+
+    /*
+     * @brief This method is used to set the players name to the players name text.
+     * 
+     * @param void
+     * 
+     * @return void
+     */
+    private void DisplayPlayersName()
+    {
+        _playersNameText.text = "Players :\n" + _networkWaitingRoomManager.GetPlayersNames();
+    }
+
+    #region NetworkManagerCallbacks
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(_lobbyScene);
+    }
+
+    public override void OnPlayerLeftRoom()
+    {
+        Debug.Log("Player left room need to redisplay the code to the new host");
+        DisplayRoomCode();
+        DisplayPlayersName();
+        DisplayTotalPlayer();
+    }
+
+    public override void OnPlayerEnteredRoom()
+    {
+        Debug.Log("Player entered room need to redisplay the code to the new host");
+        DisplayPlayersName();
+        DisplayTotalPlayer();
+    }
+    #endregion
+}
