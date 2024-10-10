c690f9494b3a5b0ac2be0ab37a1447f0b00379df
jovanHillion <jovan.hillion@epitech.eu>
2024-10-10 16:54:53 +0200

    feat(sceneManager): Add a scene manager
    Add a simple scene manager to load scene asynchronously for future use, also replace loading scene by this one exept for loadLevel call made with Pun2
    
    Feat #7,#9

---
+++ Vermines/Assets/Scripts/Scene/Loader/SceneLoader.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Sceneloader
+{
+    /*
+     * @brief This class is used to access coroutine.
+     */
+    private class LoadingMonoBehaviour : MonoBehaviour { }
+
+    /*
+     * @brief This enum is used to store the scene name.
+     */
+    public enum Scene
+    {
+        Menu,
+        Lobby,
+        // WaitingRoom - > Is loaded by the RoomManager using Pun2
+        // Game - > Is loaded by the WaitingRoomManager using Pun2
+    }
+
+    /*
+     * @brief This method is used to load the scene asynchronously.
+     * 
+     * @param Scene scene
+     * 
+     * @return void
+     */
+    public static void LoadScene(Scene scene)
+    {
+        GameObject loadingMonoBehaviour =  new GameObject("Scene Loader");
+
+        loadingMonoBehaviour.AddComponent<LoadingMonoBehaviour>().StartCoroutine(LoadSceneAsync(scene));
+
+    }
+
+    /*
+     * @brief This async method is used to load the scene asynchronously, it run each frame to check if the scene is loaded.
+     *
+     * @param Scene sceneName
+     * 
+     * @return IEnumerator
+     */
+    private static IEnumerator LoadSceneAsync(Scene sceneName)
+    {
+
+        Debug.Log("Loading scene : " + sceneName.ToString());
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName.ToString());
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
