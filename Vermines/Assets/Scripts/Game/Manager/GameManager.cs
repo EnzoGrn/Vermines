60f93eaa364d004f994466c65206cd5159163081
Enzo Garnier <enzogrn13@gmail.com>
2024-10-27 16:04:23 +0900

    feat(Game Logic): Add turn logic
    Add full turn logic with player change, but for now only eloquence gain and end of turn draw.

--- Vermines/Assets/Scripts/Game/Manager/GameManager.cs
+++ Vermines/Assets/Scripts/Game/Manager/GameManager.cs
@@ -16,8 +16,9 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
     private Dictionary<int, PlayerController> _Players = new();
 
     private bool _IsGameStarted = false;
+	private int _currentPlayerIndex = 0;
 
-    public CardManager CardManager;
+	public CardManager CardManager;
     public GameInfo    GameInfo;
 
     #endregion
@@ -26,12 +27,13 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
 
     private Action _EveryPlayersLoadCallbacks;
     private Action _InitializationOfPlayersDeck;
+	private Action _StartTurnAction;
 
-    #endregion
+	#endregion
 
-    #region Methods
+	#region Methods
 
-    public void Awake()
+	public void Awake()
     {
         if (Instance == null)
             Instance = this;
@@ -47,30 +49,86 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
         _EveryPlayersLoadCallbacks?.Invoke();   // Call this event until all players have joined the game room, after that it will be removed from the callback list.
         _InitializationOfPlayersDeck?.Invoke(); // Call this event client are ready to initialize their deck.
     }
-    
+
     public void Update()
     {
         if (!_IsGameStarted)
             return;
-        _StartingDraw?.Invoke();
-    }
+		_StartingDraw?.Invoke();
+		_StartTurnAction?.Invoke();
+	}
 
-    private Action _StartingDraw;
+	private Action _StartingDraw;
 
     private void DrawCardBegin()
     {
         PlayerController.localPlayer.DrawCard(2);
 
         _StartingDraw -= DrawCardBegin;
+        // TODO: Gain 2 eloquence for the player.
+        // TODO: Card effect for gaining eloquence.
+        // TODO: Market phase.
+        // TODO: End of the turn.
     }
 
-    /*
+    private void StartTurn()
+	{
+		var currentPlayer = GetCurrentPlayer();
+		if (currentPlayer != null)
+		{
+            Debug.Log("Start turn for player " + currentPlayer.Profile.Nickname);
+			currentPlayer.Eloquence += 2;
+            currentPlayer.Sync();
+			Debug.Log("Player eloquence: " + currentPlayer.Eloquence);
+
+			// TODO: Implement other phases like card activation, market phase, etc.
+			// End the turn and trigger the next player's turn in the NextTurn method or similar
+			// /!\ It's better if EndTurn is called by clicking on a button or something like that, for now it's called automatically
+			//EndTurn();
+		}
+
+		_StartTurnAction -= StartTurn;
+	}
+
+	private PlayerController GetCurrentPlayer()
+	{
+		return _Players.TryGetValue(_currentPlayerIndex, out PlayerController player) ? player : null;
+        //return PlayerController.LocalPlayer;
+	}
+
+	private void NextTurn()
+	{
+		_currentPlayerIndex = (_currentPlayerIndex + 1) % _Players.Count;
+
+        //_Players[i].Profile.PlayerID;
+
+		SyncGameManager("ChangeTurn");
+	}
+
+	private void EndTurn()
+	{
+		var currentPlayer = GetCurrentPlayer();
+
+		// TODO: Implement market refilling logic
+
+		currentPlayer.DrawCard(3);
+        Debug.Log("End turn for player " + currentPlayer.Profile.Nickname);
+
+		NextTurn();
+	}
+
+	private void ChangeTurn()
+	{
+		_StartTurnAction += StartTurn;
+	}
+
+	/*
      * @brief Event called when all players have joined the game room.
      * If used for initialization, it should be removed from the callback list.
-     * 
+     *
      * @note Only the master client can call this event.
      */
-    private void OnAllPlayersJoined()
+	private void OnAllPlayersJoined()
     {
         PlayerController[] controllers = FindObjectsOfType<PlayerController>();
 
@@ -85,8 +143,10 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
                 if (controllers[i].Family == CardType.None)
                     return;
                 cardTypes.Add(controllers[i].Family);
-            }
-            GameInfo.FamilyPlayed = cardTypes;
+
+				_Players.Add(i, controllers[i]); // Currently the players are sort by an index number, maybe we should sort them by their playerID
+			}
+			GameInfo.FamilyPlayed = cardTypes;
 
             _Command = "ClientReady";
 
@@ -101,7 +161,7 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
                 controllers[i].Eloquence = startingEloquence;
                 controllers[i].Sync();
 
-                _Players.Add(i, controllers[i]); // Currently the players are sort by an index number, maybe we should sort them by their playerID
+				_Players.Add(i, controllers[i]); // Currently the players are sort by an index number, maybe we should sort them by their playerID
             }
         }
 
@@ -124,8 +184,11 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
             SyncGameManager(_Command);
         }
         _InitializationOfPlayersDeck -= InitPlayerDeck;
-        _StartingDraw                += DrawCardBegin;
-        _IsGameStarted = true;
+		_StartingDraw                += DrawCardBegin;
+		_IsGameStarted = true;
+
+		if (PhotonNetwork.IsMasterClient)
+			ChangeTurn();
     }
 
     #endregion
@@ -153,26 +216,34 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
     [PunRPC]
     public void RPC_SyncGameManager(string command)
     {
-        if (!string.IsNullOrEmpty(command)) {
+		Debug.Log("Received RPC command: " + command);
+		if (!string.IsNullOrEmpty(command)) {
             _Command = command;
 
             HandleCommand();
         }
-    }
+	}
 
     private void HandleCommand()
     {
         if (_Command == "ClientReady")
             _InitializationOfPlayersDeck += InitPlayerDeck;
-    }
+		else if (_Command == "ChangeTurn")
+		{
+			Debug.Log("Changing turn...");
+			ChangeTurn();
+		}
+	}
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting) {
             stream.SendNext(_Command);
-        } else {
+			stream.SendNext(_currentPlayerIndex);
+		} else {
             _Command = (string)stream.ReceiveNext();
-        }
+			_currentPlayerIndex = (int)stream.ReceiveNext();
+		}
     }
 
     #endregion
