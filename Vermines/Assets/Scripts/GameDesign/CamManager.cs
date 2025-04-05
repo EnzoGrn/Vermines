faad293ae2f4c4a0301ec86374afd31337508b3f
jovanHillion <jovan.hillion@epitech.eu>
2025-04-05 14:47:08 +0900

    feat(CamsManager): Add sacrifice location
    Add sacrifice location and the physical environment for this one, also add some fix for hover on locations

--- Vermines/Assets/Scripts/GameDesign/CamManager.cs
+++ Vermines/Assets/Scripts/GameDesign/CamManager.cs
@@ -30,6 +30,10 @@ public class CamManager : MonoBehaviourSingleton<CamManager>
     private int _Offset = 0;
     private bool _IsAnimated = false;
 
+    // This value is used to go on special location from the none a market
+    // If different of 0 must be a special location to go after completed first cam animation to return in the none location
+    private int _GoOnSpecialLocation = 0;
+
     #endregion
 
     #region Methods
@@ -73,7 +77,7 @@ public class CamManager : MonoBehaviourSingleton<CamManager>
         if (splineID > ((int)CamSplineType.MainViewToCourtyard - 1) && _SplineID > ((int)CamSplineType.MainViewToCourtyard - 1))
         {
             // splineId - LastNoneShop + (LastShop - splineId)
-            _Offset = splineID - (int)CamSplineType.None + ((int)CamSplineType.MainViewToMarket - splineID);
+            _Offset = splineID - (int)CamSplineType.MainViewToSacrifice + ((int)CamSplineType.MainViewToMarket - splineID);
 
             StartSplineCamAnimation(splineID + _Offset);
             // Set the _SplineID not to the calculated one, but the actual one (it allows the code to do this calcul again and knows where we are)
@@ -126,16 +130,52 @@ public class CamManager : MonoBehaviourSingleton<CamManager>
         if (_IsAnimated) return;
         OnSplineAnimationRequest((int)CamSplineType.None);
     }
+
+    public void GoOnSacrificeLocation()
+    {
+        if (_IsAnimated) return;
+
+        // Check if current location is not none, if it is just go the the sacrifice location else return to none before going to sacrifice
+
+        if (_SplineID != 0)
+        {
+            OnSplineAnimationRequest((int)CamSplineType.None);
+            _GoOnSpecialLocation = (int)CamSplineType.MainViewToSacrifice;
+        }
+        else
+        {
+            OnSplineAnimationRequest((int)CamSplineType.MainViewToSacrifice);
+        }
+    }
+
     public void GoOnMarketLocation()
     {
         if (_IsAnimated) return;
-        OnSplineAnimationRequest((int)CamSplineType.MainViewToMarket);
+
+        if (_SplineID == (int)CamSplineType.MainViewToSacrifice)
+        {
+            OnSplineAnimationRequest((int)CamSplineType.None);
+            _GoOnSpecialLocation = (int)CamSplineType.MainViewToMarket;
+        }
+        else
+        {
+            OnSplineAnimationRequest((int)CamSplineType.MainViewToMarket);
+        }
     }
 
     public void GoOnCourtyardLocation()
     {
         if (_IsAnimated) return;
-        OnSplineAnimationRequest((int)CamSplineType.MainViewToCourtyard);
+        
+        if (_SplineID == (int)CamSplineType.MainViewToSacrifice)
+        {
+            OnSplineAnimationRequest((int)CamSplineType.None);
+            _GoOnSpecialLocation = (int)CamSplineType.MainViewToCourtyard;
+        }
+        else
+        {
+            OnSplineAnimationRequest((int)CamSplineType.MainViewToCourtyard);
+        }
     }
 
     public void ProceedLookAtRequest()
@@ -156,13 +196,32 @@ public class CamManager : MonoBehaviourSingleton<CamManager>
 
         OnCamLocationChanged.Invoke((CamSplineType)_SplineID);
 
-        if ((CamSplineType)_SplineID ==  CamSplineType.MainViewToCourtyard || (CamSplineType)_SplineID == CamSplineType.MarketToCourtyard)
+        if (_GoOnSpecialLocation != 0)
         {
-            ShopManager.instance.OpenCourtyard();
+            OnSplineAnimationRequest(_GoOnSpecialLocation);
+            _GoOnSpecialLocation = (int)CamSplineType.None;
         }
-        else if ((CamSplineType)_SplineID == CamSplineType.MainViewToMarket || (CamSplineType)_SplineID == CamSplineType.CourtyardToMarket)
+
+        if (!ShopManager.instance)
+            return;
+
+        switch ((CamSplineType)_SplineID)
         {
-            ShopManager.instance.OpenMarket();
+            case CamSplineType.MainViewToCourtyard:
+                ShopManager.instance.OpenCourtyard();
+                break;
+            case CamSplineType.MarketToCourtyard:
+                ShopManager.instance.OpenCourtyard();
+                break;
+            case CamSplineType.MainViewToMarket:
+                ShopManager.instance.OpenMarket();
+                break;
+            case CamSplineType.CourtyardToMarket:
+                ShopManager.instance.OpenMarket();
+                break;
+            case CamSplineType.MainViewToSacrifice:
+                // TODO: Call Open Sacrifice
+                break;
         }
     }
 
