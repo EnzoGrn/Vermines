1ce1c1a46d1bcf6225125fc34ae5dcb9b17b6354
Enzo Garnier <enzogrn13@gmail.com>
2025-09-23 11:27:08 +0200

    feat(UI): link earn eloquence to the popup

--- Vermines/Assets/Scripts/Gameplay/Logic/Phase/GainPhase.cs
+++ Vermines/Assets/Scripts/Gameplay/Logic/Phase/GainPhase.cs
@@ -8,11 +8,20 @@ namespace Vermines.Gameplay.Phases {
     using Vermines.CardSystem.Data.Effect;
     using Vermines.CardSystem.Elements;
     using Vermines.CardSystem.Enumerations;
+    using Vermines.Gameplay.Cards.Effect;
     using Vermines.Gameplay.Commands.Cards.Effects;
     using Vermines.Gameplay.Phases.Enumerations;
     using Vermines.Player;
     using Vermines.UI;
     using Vermines.UI.Screen;
+
+    public struct GainSummaryData
+    {
+        public int BaseValue;
+        public int FollowerValue;
+        public int EquipmentValue;
+        public int Total => BaseValue + FollowerValue + EquipmentValue;
+    }
 
     public class GainPhase : APhase {
 
@@ -23,6 +32,7 @@ namespace Vermines.Gameplay.Phases {
         #endregion
 
         private PlayerRef _CurrentPlayer;
+        private GainSummaryData _gainSummary;
 
         #region Override Methods
 
@@ -37,7 +47,10 @@ namespace Vermines.Gameplay.Phases {
 
             ExecuteCardEffect();
 
-            ICommand earnCommand = new EarnCommand(_CurrentPlayer, GameManager.Instance.SettingsData.NumberOfEloquencesEarnInGainPhase, DataType.Eloquence);
+            _gainSummary.BaseValue = GameManager.Instance.SettingsData.NumberOfEloquencesEarnInGainPhase;
+            _gainSummary.FollowerValue = CalculateFollowerBonus(player);
+
+            ICommand earnCommand = new EarnCommand(_CurrentPlayer, _gainSummary.BaseValue, DataType.Eloquence);
 
             CommandInvoker.ExecuteCommand(earnCommand);
 
@@ -50,9 +63,30 @@ namespace Vermines.Gameplay.Phases {
 
                 if (gameplayUIController != null) {
                     gameplayUIController.GetActiveScreen(out GameplayUIScreen lastScreen);
-                    gameplayUIController.Show<GameplayUIGainSummary>(lastScreen);
+                    gameplayUIController.ShowWithParams<GameplayUIGainSummary, GainSummaryData>(_gainSummary,lastScreen);
                 }
             }
+        }
+
+        private int CalculateFollowerBonus(PlayerRef player)
+        {
+            int total = 0;
+            var deck = GameDataStorage.Instance.PlayerDeck[player];
+            foreach (ICard card in deck.PlayedCards)
+            {
+                foreach (IEffect effect in card.Data.Effects)
+                {
+                    if (effect.Type == EffectType.Play && 
+                        effect.SubEffect != null &&
+                        effect.SubEffect is EarnEffect eloquenceBonus &&
+                        eloquenceBonus.DataToEarn == DataType.Eloquence)
+                    {
+                        Debug.LogFormat("AAAAa");
+                        total += eloquenceBonus.Amount;
+                    }
+                }
+            }
+            return total;
         }
 
         public override void Reset()
